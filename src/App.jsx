470a6e1b3092c65119e950f83b83e3c7635b82f0
Riviets <vladvladpobuta@gmal.com>
2025-04-09 15:51:06 +0300

    Implemented Navbar component logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
 
 const App = () => {
 
   return (
-    <>
-    </>
+    <main className="bg-black min-h-screen">
+      <Navbar />
+      {/* <Hero />
+      <Highlights /> */}
+    </main>
   )
 }
 
